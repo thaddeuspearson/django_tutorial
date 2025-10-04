73313236080132799eec00d247b3730815ea325d
Thaddeus <thaddeuspearson@gmail.com>
2025-10-05 01:56:23 +0200

    + templates, views for detail, finish Tutorial 3

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Question
 
 
@@ -9,7 +9,8 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request, question_id):
