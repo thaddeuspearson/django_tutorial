066863ea79ac7a768c3662a5f4539658613a2065
Thaddeus <thaddeuspearson@gmail.com>
2025-10-05 23:17:16 +0200

    + settings for eb

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,17 +20,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-aj_sj4zym@ef(^juj(-@#6nydar90%evmo=+ri7w=5g%yej_w@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "django-env.eba-ahgdx2q5.us-east-1.elasticbeanstalk.com"
+]
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    "polls.apps.PollsConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
