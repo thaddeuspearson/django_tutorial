64f2909e6a27e42174bf67e853a0c875ca937da6
Thaddeus <thaddeuspearson@gmail.com>
2025-10-20 01:07:09 +0200

    EB config for SQLite in temp

--- mysite/settings.py
+++ mysite/settings.py
@@ -81,9 +81,10 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db" / "db.sqlite3",
+        "NAME": "/tmp/db.sqlite3",
     }
 }
+
 
 
 # Password validation
